c234681e814c915e91443cced1857de8c6036bc9
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-27 21:32:10 -0700

    Add best practice solution

--- lib/add_binary.rb
+++ lib/add_binary.rb
@@ -1,10 +1,14 @@
 require 'pry'
 
-def add_binary(a, b)
-  sum = a + b
-  get_binary(sum)
-end
+# def add_binary(a, b)
+#   sum = a + b
+#   get_binary(sum)
+# end
+#
+# def get_binary(number)
+#   number.to_s(2)
+# end
 
-def get_binary(number)
-  number.to_s(2)
-end
+# BEST PRACTICE
+
+def add_binary(a, b) (a+b).to_s(2) end
