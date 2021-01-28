e5c37b48c2fb9166de133e71808269ecd27604ed
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-27 21:24:34 -0700

    Add get_binary method

--- lib/add_binary.rb
+++ lib/add_binary.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
 def add_binary(a, b)
+  sum = a + b
+  get_binary(sum)
+end
+
+def get_binary(number)
 
 end
--- test/add_binary_test.rb
+++ test/add_binary_test.rb
@@ -12,3 +12,7 @@ class AddBinaryTest < Minitest::Test
     assert_equal add_binary(51,12), "111111"
   end
 end
+
+# Implement a function that adds two numbers together and returns their sum in binary.
+# The conversion can be done before, or after the addition.
+# The binary number returned should be a string.
