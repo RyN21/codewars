b4016429ad05dc6da73ae2e762bb4454f3f228c6
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-27 21:13:44 -0700

    Add test for add_binary function

---
+++ test/add_binary_test.rb
@@ -0,0 +1,14 @@
+gem 'minitest', '~>5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/add_binary'
+
+class AddBinaryTest < Minitest::Test
+  def test_it_adds_binary
+    assert_equal add_binary(1,1), "10"
+    assert_equal add_binary(0,1), "1"
+    assert_equal add_binary(1,0), "1"
+    assert_equal add_binary(2,2), "100"
+    assert_equal add_binary(51,12), "111111"
+  end
+end
