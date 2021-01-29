801321ef3ad3524d9fbde46b0e1eda3a2fc46f53
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-28 21:21:25 -0700

    Worked on ticket challenge

--- lib/tickets.rb
+++ lib/tickets.rb
@@ -2,14 +2,35 @@ require 'pry'
 
 def tickets(people)
   drawer = 0
-  people.each do |p|
-    if p = 25
-      drawer += p
-    elsif p == 50 && drawer >= 25
-      binding.pry
-      drawer += p
+  until people == [] do
+    if people[0] == 25
+      drawer += people[0]
+      return 'YES' if people = []
+    elsif people[0] == 50 && drawer >= 25
+      drawer += 50
       drawer -= 25
-
+      return 'YES' if people = []
+    elsif people[0] == 100 && drawer >= 75
+      drawer += 100
+      drawer -= 75
+      return 'YES' if people = []
+    else
+      return 'NO'
     end
   end
+  people.shift
 end
+
+
+
+
+
+
+# people.each do |p|
+#   if p = 25
+#     drawer += p
+#   elsif p == 50 && drawer >= 25
+#     binding.pry
+#     drawer += p
+#     drawer -= 25
+#   end
