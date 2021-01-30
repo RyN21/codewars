0790a1b68f884acc20a7d33b4d6bcf6ec3a8e508
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-29 20:02:56 -0700

    made some progress i think

--- lib/tickets.rb
+++ lib/tickets.rb
@@ -1,26 +1,39 @@
 require 'pry'
 
+
 def tickets(people)
-  drawer = 0
-  until people == [] do
-    if people[0] == 25
-      drawer += people[0]
-      people.shift
-      return 'YES' if people = []
-    elsif people[0] == 50 && drawer >= 25
-      drawer += 25
-      people.shift
-      return 'YES' if people = []
-    elsif people[0] == 100 && drawer >= 75
-      drawer += 25
-      people.shift
-      return 'YES' if people = []
-    else
-      break
-    end
-    return 'NO'
+  drawer = []
+  until people.count == 1
+    drawer << people[0]
+    people.shift
+    binding.pry
+
   end
 end
+
+
+
+# def tickets(people)
+#   drawer = 0
+#   until people == [] do
+#     if people[0] == 25
+#       drawer += people[0]
+#       people.shift
+#       return 'YES' if people = []
+#     elsif people[0] == 50 && drawer >= 25
+#       drawer += 25
+#       people.shift
+#       return 'YES' if people = []
+#     elsif people[0] == 100 && drawer >= 75
+#       drawer += 25
+#       people.shift
+#       return 'YES' if people = []
+#     else
+#       break
+#     end
+#     return 'NO'
+#   end
+# end
 
 
 
