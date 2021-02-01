f5e75256006f2fab05dd49f6be576dc005525300
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-31 21:47:09 -0700

    Add pig it test

---
+++ test/simple_pig_latin_test.rb
@@ -0,0 +1,14 @@
+gem 'minitest', '~>5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/simple_pig_latin'
+
+class PigItTest < Minitest::Test
+  def test_it_adds_binary
+    assert_equal pig_it('Pig latin is cool'), 'igPay atinlay siay oolcay'
+    assert_equal pig_it('This is my string'), 'hisTay siay ymay tringsay'
+  end
+end
+
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+# Leave punctuation marks untouched.
