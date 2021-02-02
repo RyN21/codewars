9727990cf77d71b15873ae519a11fe75da955d42
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-01 21:44:18 -0700

    Add to challenge

--- lib/simple_pig_latin.rb
+++ lib/simple_pig_latin.rb
@@ -3,13 +3,21 @@ require 'pry'
 def pig_it(string)
   special = "?<>',?[]}{=-)(*&^%$#`~{}"
   string.split(" ").map do |w|
-    if !special.include?(w)
-      n = w.chars
-      n << n.shift
-      n << "ay"
-      n.join
-    elsif special.include?(w)
-      w
+    if w.index( /[^[:alnum:]]/ ) == nil
+      a = w.chars
+      a << a.shift
+      a << "ay"
+      a.join
+    else
+      binding.pry
+      i = w.index( /[^[:alnum:]]/ )
+      a = w.chars
+      special = a[i]
+      a.delete_at(i)
+      a << a.shift
+      a << "ay"
+      a << special
+      a.join
     end
   end.join(" ")
 end
