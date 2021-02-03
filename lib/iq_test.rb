9f077797f3a6d9491c4d88940c4acdb81c0a782b
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-02 22:33:10 -0700

    Add additional solutions

--- lib/iq_test.rb
+++ lib/iq_test.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+# MY SOLUTION
+#=======================================================================================
+
 def iq_test(nums)
   even = []
   odd = []
@@ -16,5 +19,41 @@ def iq_test(nums)
     num = odd[0]
     index = nums.split().index(num)
     index + 1
+  end
+end
+
+
+# BEST PRACTICE  && CLEVER
+#=======================================================================================
+
+def iq_test(numbers)
+  nums = numbers.split.map(&:to_i).map(&:even?)
+  nums.count(true) > 1 ? nums.index(false) + 1 : nums.index(true) + 1
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def iq_test(numbers)
+  ar = numbers.split.map(&:to_i)
+  ar.index(ar.partition(&:even?).find(&:one?).first) + 1
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def iq_test(numbers)
+  arr = numbers.split(' ').map{|e| e.to_i}
+  evenCount = arr.select {|e| e.even?}.count
+  oddCount = arr.select {|e| e.odd?}.count
+
+  arr.each_with_index do |e,i|
+    if evenCount > oddCount
+      return i + 1 if e.odd?
+    else
+      return i + 1 if e.even?
+    end
   end
 end
