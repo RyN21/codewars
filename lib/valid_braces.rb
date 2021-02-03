fb027edb24e8eae09a59a418e8c2a0657439584b
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-02 21:11:22 -0700

    Add other solutions

--- lib/valid_braces.rb
+++ lib/valid_braces.rb
@@ -10,3 +10,52 @@ def validBraces(str)
   return false if s == str
   validBraces(s)
 end
+
+
+# BEST PRACTICE  &&  CLEVER
+#=======================================================================================
+
+def validBraces(braces)
+  while braces.gsub!(/(\(\)|\[\]|\{\})/,'') do; end
+  braces.empty?
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def validBraces(braces)
+
+  (braces.length / 2).times do |i|
+    braces.sub!("{}", "")
+    braces.sub!("[]", "")
+    braces.sub!("()", "")
+  end
+  braces.empty?
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def validBraces(braces)
+  while braces.gsub!(/(\(\)|\[\]|\{\})/,'') do; end
+  braces.empty? ? true : false
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+BRACES = { "(" => ")", "[" => "]", "{" => "}" }
+def validBraces(braces)
+  braces.each_char.each_with_object([]) { |char, stack|
+    case char
+    when *BRACES.keys
+      stack.push(BRACES[char])
+    when *BRACES.values
+      return false unless stack.last == char
+      stack.pop
+    end
+  }.empty?
+end
