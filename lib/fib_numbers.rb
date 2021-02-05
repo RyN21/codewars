0c23d758afc0de5c9623d8dc90b3c2a12163cac3
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-04 17:10:07 -0700

    Still working on the problem

--- lib/fib_numbers.rb
+++ lib/fib_numbers.rb
@@ -3,12 +3,15 @@ require 'pry'
 def productFib(prod)
   fibs = [0, 1]
   i = 0
-  50.times do
+  100.times do
     num = fibs[i] + fibs[i + 1]
     fibs << num
     i += 1
   end
   binding.pry
-  
-  prod.to_f / fibs[25]
+  i = 0
+  until prod == 
+  if prod prod.to_f / fibs[i] == 0 || prod prod.to_f / fibs[i] == 1
+    i +=
+  if
 end
