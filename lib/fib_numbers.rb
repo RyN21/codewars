f44585356cdffa019b0c4d656b07084fc4816590
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-04 17:06:58 -0700

    Add functionality to create an array of fib numbers

--- lib/fib_numbers.rb
+++ lib/fib_numbers.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def productFib(prod)
-
+  fibs = [0, 1]
+  i = 0
+  50.times do
+    num = fibs[i] + fibs[i + 1]
+    fibs << num
+    i += 1
+  end
+  binding.pry
+  
+  prod.to_f / fibs[25]
 end
