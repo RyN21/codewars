da17ff3a1f2472bb1252a318f9965b84843d3a93
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-05 23:42:54 -0700

    Refactor

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -4,11 +4,10 @@ require 'pry'
 #=======================================================================================
 
 def rgb(r, g, b)
-  arr = [r, g, b]
-  arr.map do |ele|
-    ele < 0 ? ele = 0 : ele = ele
-    ele > 255 ? ele = 255 : ele = ele
-    ele.to_s(16).upcase.rjust(2, "0")
+  [r, g, b].map do |e|
+    e < 0 ? e = 0 : e = e
+    e > 255 ? e = 255 : e = e
+    e.to_s(16).upcase.rjust(2, "0")
   end.join
 end
 
