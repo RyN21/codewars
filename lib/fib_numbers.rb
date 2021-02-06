3d4df001c935520690fd1b88345d339465b048b4
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-05 22:48:37 -0700

    Tests pass!

--- lib/fib_numbers.rb
+++ lib/fib_numbers.rb
@@ -1,15 +1,14 @@
 require 'pry'
 
 def productFib(prod)
-  i = 1
-  p = 1
-  fib = [0, 1, 1]
+  i = 0
+  p = 0
+  fib = [0, 1]
   until prod == p || prod < p
     new_fib = fib[i] + fib[i+1]
     fib << new_fib
     p = fib[i] * fib[i+1]
     i += 1
   end
-  return [fib[i], fib[i+1], true] if p == prod
-  return [fib[i], fib[i+1], false] if p > prod
+  p == prod ? [fib[i - 1], fib[i], true] : [fib[i - 1], fib[i], false]
 end
