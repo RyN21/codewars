a9c2827aef969598b5478f01e051c63b1da4b8ae
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-05 23:51:05 -0700

    Fix typo

--- test/greed_is_good_test.rb
+++ test/greed_is_good_test.rb
@@ -5,15 +5,15 @@ require_relative '../lib/greed_is_good'
 
 class GreedIsGoodTest < Minitest::Test
   def test_it_should_value_this_as_worthless
-    assert_equal score( [2, 3, 4, 6, 2] ) == 0,   "Should be 0 :-(" )
+    assert_equal score( [2, 3, 4, 6, 2] ) == 0,   "Should be 0 :-("
   end
 
   def test_it_should_value_this_triplet_correctly
-    assert_equal score( [2, 2, 2, 3, 3] ) == 200, "Should be 200" )
+    assert_equal score( [2, 2, 2, 3, 3] ) == 200, "Should be 200"
   end
 
   def test_it_should_value_this_mixed_set_correctly
-    assert_equal score( [2, 4, 4, 5, 4] ) == 450, "Should be 450" )
+    assert_equal score( [2, 4, 4, 5, 4] ) == 450, "Should be 450"
   end
 end
 
