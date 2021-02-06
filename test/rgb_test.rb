4e09c1a0984e5d880297b69bbb471697bcfaf641
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-05 22:56:05 -0700

    Add instructions

--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -11,3 +11,11 @@ class RgbTest < Minitest::Test
     assert_equal rgb(173,255,47), 'ADFF2F')
   end
 end
+
+
+# The rgb function is incomplete. Complete it so that passing in RGB decimal values
+# will result in a hexadecimal representation being returned.
+# Valid decimal values for RGB are 0 - 255. Any values that fall out of that
+# range must be rounded to the closest valid value.
+
+# Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
