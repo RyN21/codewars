d72ae0be2f49499b70537d51312f8f54b6d63fbf
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-06 15:55:54 -0700

    Tests pass

--- lib/greed_is_good.rb
+++ lib/greed_is_good.rb
@@ -4,32 +4,25 @@ require 'pry'
 #=======================================================================================
 
 def score(dice)
-  binding.pry
-  # scr = 0
-  # points = {'3x1s' => 1000, '3x6s' => 600, '3x5s' => 500, '3x4s' => 400, '3x3s' => 300, '3x2s' => 200, '1x1' => 100, '1x5' => 50}
-  # grouped = dice.sort.chunk do |e|
-  #   e
-  # end
-  #
-  # grouped.each do |g|
-  #   scr += points["3x#{g[0]}s"] if g[1].count >= 3
-  #   3.times {g[1].shift}
-  #   scr += points["3x#{g[0]}s"] if g[1].count >= 3
-  #   3.times {g[1].shift}
-  #
-  #   if g[0] == 5 || g[0] == 1
-  #   end
-  #   # scr += points["1x1"] if !g[1].empty? && g[0] == 1
-  #   # 1.times {g[1].shift}
-  #   # scr += points["1x1"] if !g[1].empty? && g[0] == 1
-  #   # 1.times {g[1].shift}
-  #   #
-  #   # scr += points["1x5"] if !g[1].empty? && g[0] == 5
-  #   # 1.times {g[1].shift}
-  #   # scr += points["1x5"] if !g[1].empty? && g[0] == 5
-  #   # 1.times {g[1].shift}
-  # end
-  # scr
+  scr = 0
+  points = {'3x1s' => 1000, '3x6s' => 600, '3x5s' => 500, '3x4s' => 400, '3x3s' => 300, '3x2s' => 200, '1x1' => 100, '1x5' => 50}
+  grouped = dice.sort.chunk do |e|
+    e
+  end.select{ |e, chunk| chunk.size > 0 }
+
+  grouped.each do |g|
+    scr += points["3x#{g[0]}s"] if g[1].count >= 3
+    3.times {g[1].shift} if g[1].count >= 3
+    scr += points["3x#{g[0]}s"] if g[1].count == 3
+    3.times {g[1].shift} if g[1].count >= 3
+    if g[0] == 5 || g[0] == 1
+      until g[1].empty?
+        scr += points["1x#{g[0]}"]
+        g[1].shift
+      end
+    end
+  end
+  scr
 end
 
 
--- test/greed_is_good_test.rb
+++ test/greed_is_good_test.rb
@@ -4,13 +4,13 @@ require 'minitest/pride'
 require_relative '../lib/greed_is_good'
 
 class GreedIsGoodTest < Minitest::Test
-  # def test_it_should_value_this_as_worthless
-  #   assert_equal score( [2, 3, 4, 6, 2] ), 0
-  # end
-  #
-  # def test_it_should_value_this_triplet_correctly
-  #   assert_equal score( [2, 2, 2, 3, 3] ), 200
-  # end
+  def test_it_should_value_this_as_worthless
+    assert_equal score( [2, 3, 4, 6, 2] ), 0
+  end
+
+  def test_it_should_value_this_triplet_correctly
+    assert_equal score( [2, 2, 2, 3, 3] ), 200
+  end
 
   def test_it_should_value_this_mixed_set_correctly
     assert_equal score( [2, 4, 4, 5, 4] ), 450
