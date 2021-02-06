ac280ceb618bcf43a4dd60d8799fd8a7aba5c255
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-05 22:53:03 -0700

    Add other examples

--- lib/fib_numbers.rb
+++ lib/fib_numbers.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+# MY SOLUTION
+#=======================================================================================
+
 def productFib(prod)
   i = 0
   p = 0
@@ -11,4 +14,46 @@ def productFib(prod)
     i += 1
   end
   p == prod ? [fib[i - 1], fib[i], true] : [fib[i - 1], fib[i], false]
+end
+
+
+# BEST PRACTICE  &&  CLEVER
+#=======================================================================================
+
+def productFib(prod)
+  a, b = [0, 1]
+  while prod > a * b
+    a, b = [b, a + b]
+  end
+  [a, b, prod == a * b]
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def productFib(prod)
+  a, b = 1, 0
+  a, b = a + b, a while a * b < prod
+  [b, a, a * b == prod]
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def productFib(prod)
+  first, second = *consecutive_fibonacci_numbers.find { |a, b| a * b >= prod }
+  [first, second, first * second == prod]
+end
+
+def consecutive_fibonacci_numbers
+  Enumerator.new do |yielder|
+    a, b = 1, 1
+
+    loop do
+      yielder.yield([a, b])
+      a, b = b, a + b
+    end
+  end
 end
