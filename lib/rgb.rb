432f0c5495d9b57e02e8491c54ca2433af6cc55a
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-05 23:14:45 -0700

    Still working on it

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,7 +1,13 @@
 require 'pry'
 
 def rgb(r, g, b)
-  
+  if r > 255 then r = 255 end
+  if g > 255 then g = 255 end
+  if b > 255 then b = 255 end
+  if r < 0 then r = 0 end
+  if g < 0 then g = 0 end
+  if b < 0 then b = 0 end
+  r, g, b =
   newr = r.to_s(16).upcase.rjust(2, "0")
   newg = g.to_s(16).upcase.rjust(2, "0")
   newb = b.to_s(16).upcase.rjust(2, "0")
