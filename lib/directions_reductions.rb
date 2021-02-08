9560c5cd3b8c29b1ba86b598b9c290dd184b7f2c
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-08 15:49:14 -0700

    Play through the problem

--- lib/directions_reductions.rb
+++ lib/directions_reductions.rb
@@ -15,3 +15,26 @@ repeat steps 1-4
 we will want to set a limit. repeat process as long as i < dir.length
 
 then return the dir array
+
+if i == "NORTH" and i + 1 == "SOUTH" ||
+if i == "SOUTH" and i + 1 == "NORTH" ||
+if i == "WEST" and i + 1 == "EAST"
+if i == "EAST" and i + 1 == "NORTH"
+delete index i and i + 1
+
+i = 0
+["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+
+i = 0
+["SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+
+i = 1
+["SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+
+i = 1
+["SOUTH", "NORTH", "WEST"]
+
+
+
+
+["WEST", "SOUTH", "NORTH", "NORTH", "WEST"]
