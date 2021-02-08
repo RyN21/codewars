08794d8cf8d6d0c140376c72a0b9a6c67b3554a0
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-08 15:39:08 -0700

    Add psuedo code plan

--- lib/directions_reductions.rb
+++ lib/directions_reductions.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def dirReduc(dir)
-  
+  # i = 0
+  # dir.each do |d|
 end
+
+start with i = 0
+1 index of 0 and index of 1 and compare the two.
+2 delete both if 0 is north or south and 1 is north or south
+3 delete both if 0 is north or east and 1 is north or west
+4 if they arent == incrament index to look at the next
+repeat steps 1-4
+
+we will want to set a limit. repeat process as long as i < dir.length
+
+then return the dir array
