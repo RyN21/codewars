048d6eabe3ae5567b9746b00e00c4c56e0c230c0
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-09 11:23:13 -0700

    Add names

--- test/double_cola_test.rb
+++ test/double_cola_test.rb
@@ -5,6 +5,8 @@ require_relative '../lib/double_cola'
 
 class DoubleColaTest < Minitest::Test
   def test_it
+    names = ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"]
+    
     assert_equal who_is_next(names, 1), "Sheldon"
     assert_equal who_is_next(names, 52), "Penny"
     assert_equal who_is_next(names, 7230702951), "Leonard"
