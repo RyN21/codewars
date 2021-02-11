b1bcd46ab869fab1765dc17654e02a5ec207a9c6
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-10 22:39:38 -0700

    Edit test names

--- ruby/test/4kyu/nesting_structure_comp_test.rb
+++ ruby/test/4kyu/nesting_structure_comp_test.rb
@@ -1,9 +1,9 @@
 gem 'minitest', '~>5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/directions_reduction'
+require_relative '../lib/nesting_structure_comp'
 
-class DirectionsReductionTest < Minitest::Test
+class NestingStructureComparisonTest < Minitest::Test
   def test_it
     assert_equal [1,[1,1]].same_structure_as([2,[2,2]]), "[1,[1,1]] same as [2,[2,2]]"
     assert_equal ![1,[1,1]].same_structure_as([[2,2],2]), "[1,[1,1]] not same as [[2,2],2]"
