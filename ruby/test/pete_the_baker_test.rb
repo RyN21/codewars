755210475a67aaf3935d3933485071836b7ffe49
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-14 22:41:47 -0700

    Add instructions

--- ruby/test/pete_the_baker_test.rb
+++ ruby/test/pete_the_baker_test.rb
@@ -13,3 +13,13 @@ class PeteTheBakerTest < Minitest::Test
     assert_equal cakes({"cream"=>1, "flour"=>3, "sugar"=>1, "milk"=>1, "oil"=>1, "eggs"=>1},{"sugar"=>1, "eggs"=>1, "flour"=>3, "cream"=>1, "oil"=>1, "milk"=>1}), 1
   end
 end
+
+
+# Pete likes to bake some cakes. He has some recipes and ingredients.
+# Unfortunately he is not good in maths. Can you help him to find out,
+# how many cakes he could bake considering his recipes?
+
+# Write a function cakes(), which takes the recipe (object) and the available
+# ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer).
+# For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
+# Ingredients that are not present in the objects, can be considered as 0.
