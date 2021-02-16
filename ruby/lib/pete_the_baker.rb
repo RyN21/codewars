e3b5e1250ba036df2c2d4c95f4857191e0a108ab
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-15 23:09:00 -0700

    Add psuedo code

--- ruby/lib/pete_the_baker.rb
+++ ruby/lib/pete_the_baker.rb
@@ -1,3 +1,32 @@
-def cakes(recipe, available)
+require 'pry'
 
+# MY SOLUTION
+#=======================================================================================
+
+def cakes(recipe, available)
+  binding.pry
 end
+
+
+# BEST PRACTICE
+#=======================================================================================
+
+
+
+# CLEVER
+#=======================================================================================
+
+
+
+
+# PSUEDO CODE
+#=======================================================================================
+
+Iterate over the recipe to begin
+take the ingredient from the recipe amount and compare it to the available amount
+based on the comparison, determine how many cakes can be made
+then compare the rest of the ingredients and check for the same
+Based on all the comparisons, determine how many cakes can actually be made
+if an ingredient is missing then no cakes can be made
+take the smallest amount of cakes that can be made based on the ingredients amount
+to determine number of cakes that can actually be used
