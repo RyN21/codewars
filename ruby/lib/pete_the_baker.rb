ea0a0e27eb7a4a1fc64178bd764f5e9b220901bd
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-15 23:15:02 -0700

    Did a little work to the method

--- ruby/lib/pete_the_baker.rb
+++ ruby/lib/pete_the_baker.rb
@@ -4,7 +4,13 @@ require 'pry'
 #=======================================================================================
 
 def cakes(recipe, available)
-  binding.pry
+  cake = {}
+  recipe.each do |i, a|
+
+    ava_a = available[i]
+    cake[i] += 1 if ava_a > a
+    binding.pry
+  end
 end
 
 
@@ -22,11 +28,11 @@ end
 # PSUEDO CODE
 #=======================================================================================
 
-Iterate over the recipe to begin
-take the ingredient from the recipe amount and compare it to the available amount
-based on the comparison, determine how many cakes can be made
-then compare the rest of the ingredients and check for the same
-Based on all the comparisons, determine how many cakes can actually be made
-if an ingredient is missing then no cakes can be made
-take the smallest amount of cakes that can be made based on the ingredients amount
-to determine number of cakes that can actually be used
+# Iterate over the recipe to begin
+# take the ingredient from the recipe amount and compare it to the available amount
+# based on the comparison, determine how many cakes can be made
+# then compare the rest of the ingredients and check for the same
+# Based on all the comparisons, determine how many cakes can actually be made
+# if an ingredient is missing then no cakes can be made
+# take the smallest amount of cakes that can be made based on the ingredients amount
+# to determine number of cakes that can actually be used
