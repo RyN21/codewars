6f8494070dafe3f1a3e8f6c73fd80858b3730635
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-16 20:07:22 -0700

    Add tests

---
+++ ruby/test/strip_comments_test.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/strip_comments'
+
+def checkComments(input, markers, expected)
+  actual = solution(input, markers)
+  Test.assert_equals(actual, expected, "Returned '#{actual}' but expected '#{expected}'")
+end
+
+checkComments("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"], "apples, plums\npears\noranges")
+checkComments("Q @b\nu\ne -e f g", ["@", "-"], "Q\nu\ne")
