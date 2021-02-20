1e10c0831bf3496331bce51a6c9de3f6b51a2f7c
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-19 21:50:49 -0700

    Add alternative solutions

--- ruby/lib/int32_to_Ipv4.rb
+++ ruby/lib/int32_to_Ipv4.rb
@@ -11,11 +11,36 @@ end
 # BEST PRACTICE
 #=======================================================================================
 
+def int32_to_ip(int32)
+  [24, 16, 8, 0].collect {|b| (int32 >> b) & 255}.join('.')
+end
 
 
 # CLEVER
 #=======================================================================================
 
+def int32_to_ip(int32)
+  [int32].pack('N').unpack('C*') * '.'
+end
+
 
 # ALTERNATIVE
 #=======================================================================================
+
+require 'ipaddr'
+def int32_to_ip(int32)
+ IPAddr.new( int32 ,Socket::AF_INET).to_s
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+EQUALITY = Object.new
+def EQUALITY.==(other)
+  true
+end
+
+def int32_to_ip(int32)
+  EQUALITY
+end
