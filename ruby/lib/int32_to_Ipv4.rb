a0254cd3a78919f26d75b0626c0cb7900e800f11
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-19 21:49:08 -0700

    Refactored

--- ruby/lib/int32_to_Ipv4.rb
+++ ruby/lib/int32_to_Ipv4.rb
@@ -4,9 +4,7 @@ require 'pry'
 #=======================================================================================
 
 def int32_to_ip(int32)
-  array = []
-  array << int32
-  array.pack("N").unpack("CCCC").join(".")
+  Array([int32]).pack("N").unpack("CCCC").join(".")
 end
 
 
