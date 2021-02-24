f1fff7e0eb04cbfd5dc31ba0e01ddb7c32b21933
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-23 21:49:03 -0700

    Add psuedo code

--- ruby/lib/beeramid.rb
+++ ruby/lib/beeramid.rb
@@ -6,8 +6,11 @@ require 'pry'
 def beeramid(bonus, price)
   level = 1
   beers = bonus/price
+  until beers == 0
+    sq = level*level
 
-  binding.pry
+    binding.pry
+  end
 end
 
 
@@ -33,3 +36,13 @@ end
 
 # PSUEDO CODE
 #=======================================================================================
+
+# create variables for number of beers bounus - price
+# create varibale for level. Will incrament + 1
+# square the level to find out how many beers it will take for that row
+# compare beers to number needed
+# if theres enough then incrament level + 1 and subtract from beers
+ 
+# loop back to do the same thing but now the next level
+
+# if / until beers are not enough then return level - 1
