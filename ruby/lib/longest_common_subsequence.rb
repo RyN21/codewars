5f685e6a3afa827d9975b72fc04adcc002ead179
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-25 08:41:41 -0700

    Tests pass

--- ruby/lib/longest_common_subsequence.rb
+++ ruby/lib/longest_common_subsequence.rb
@@ -6,12 +6,15 @@ require 'pry'
 
 def lcs(x, y)
   ss = []
-  x.length < y.length ? s = x : s = y
+  x.length < y.length ? s = x.split("") : s = y.split("")
+  s == x ? l = y.split("") : l = x.split("")
   s.length.times do
-    s.split("").find do |e|
-      binding.pry
+    s.each do |e|
+      ss << e if l.include?(e)
+      l.delete(e) if l.include?(e)
     end
   end
+  ss.join
 end
 
 # CLEVER
@@ -37,34 +40,34 @@ end
 # PSUEDO CODE
 #=======================================================================================
 
-take two strings and return the common subsequence
-initial thought is that we are going compare the 2 strings together
-split both strings into arrays to make it more easy to work with
-so I thinking that when we compare the two arrays, we just eliminate elements that do not match
-or take the ones that are matching
-
-
-  "abcdef" "abc"
-  => abc
-
-  "132535365" "123456789"
-  "1235365" "123456789"
-  123
-
-  x = [a,b,c,d,e,f]
-  y = [a,b,c]
-
-
-
-  x = [1,3,2,5,3,5,3,6,5]
-
-  x = [1,3,2,5,3,5,3,6,5]
-  y = [1,2,3,4,5,6,7,8,9]
-
-  [1,]
-  [1,2,3,5,6]
-
-so the new game plan is to choose the smaller of the strings
-then however long the string is, do that many times and find the first
-element in the other string if it exists and throw it in an array
-at the end join the array for the new string
+# take two strings and return the common subsequence
+# initial thought is that we are going compare the 2 strings together
+# split both strings into arrays to make it more easy to work with
+# so I thinking that when we compare the two arrays, we just eliminate elements that do not match
+# or take the ones that are matching
+#
+#
+#   "abcdef" "abc"
+#   => abc
+#
+#   "132535365" "123456789"
+#   "1235365" "123456789"
+#   123
+#
+#   x = [a,b,c,d,e,f]
+#   y = [a,b,c]
+#
+#
+#
+#   x = [1,3,2,5,3,5,3,6,5]
+#
+#   x = [1,3,2,5,3,5,3,6,5]
+#   y = [1,2,3,4,5,6,7,8,9]
+#
+#   [1,]
+#   [1,2,3,5,6]
+#
+# so the new game plan is to choose the smaller of the strings
+# then however long the string is, do that many times and find the first
+# element in the other string if it exists and throw it in an array
+# at the end join the array for the new string
