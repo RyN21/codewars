4a189310540d3a19b752b3c72cf1b7e9979c9123
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-02-27 20:10:14 -0700

    Made some progress

--- ruby/lib/text_align_justify.rb
+++ ruby/lib/text_align_justify.rb
@@ -4,6 +4,10 @@ require 'pry'
 #=======================================================================================
 
 def justify(text, width)
+  text_length = text.length
+  index = 6
+  new_text = text.chars.insert(index, "\n").join
+  new_text
   binding.pry
 end
 
@@ -30,12 +34,12 @@ end
 # PSUEDO CODE  &&  INITIAL THOUGHTS
 #=======================================================================================
 
-string method ".ljust"
-takes in a string and width
-add a line break to the string at " " whitespaces only
-maybe convert string to an array
-then the 7th element add a new line... if its a whitespace
-if it is not... then back track to the begining of the array until a whitespace is found
-and then add a new line
-break each line into a possible array
-then iterate over each array and add whitespaces to them so the total length is equal the width
+# string method ".ljust"
+# takes in a string and width
+# add a line break to the string at " " whitespaces only
+# maybe convert string to an array
+# then the 7th element add a new line... if its a whitespace
+# if it is not... then back track to the begining of the array until a whitespace is found
+# and then add a new line
+# break each line into a possible array
+# then iterate over each array and add whitespaces to them so the total length is equal the width
