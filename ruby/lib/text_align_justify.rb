fc4294ccd37f7582004159d2a6634ea657b03cbf
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-01 16:59:11 -0700

    Start from scratch. Try to approach problem from a different angle

--- ruby/lib/text_align_justify.rb
+++ ruby/lib/text_align_justify.rb
@@ -4,18 +4,22 @@ require 'pry'
 #=======================================================================================
 
 def justify(text, width)
-  text_length = text.length
-  index = 6
-  new_text = text.chars.insert(index, "\n").join
-  lines = new_text.split(("\n"))
-  lines.map do |line|
-    if line.size < width
-      line.gsub(/ /, '  ').concat("\n")
-    elsif line.chars.length == 1 || line.chars.length == 2 && line.chars.include?(" ")
-      line.delete(' ')
-    end
-  end.join
+  
 end
+
+# def justify(text, width)
+#   text_length = text.length
+#   index = 6
+#   new_text = text.chars.insert(index, "\n").join
+#   lines = new_text.split(("\n"))
+#   lines.map do |line|
+#     if line.size < width
+#       line.gsub(/ /, '  ').concat("\n")
+#     elsif line.chars.length == 1 || line.chars.length == 2 && line.chars.include?(" ")
+#       line.delete(' ')
+#     end
+#   end.join
+# end
 
 # def add_lines(text, width)
 #
