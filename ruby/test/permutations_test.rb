f5ca7c2882faa8b5f0b309a3e847f016ea6ee23a
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-28 19:18:23 -0600

    Add instructions

--- ruby/test/permutations_test.rb
+++ ruby/test/permutations_test.rb
@@ -8,3 +8,15 @@ class MostFrequentlyUsedWordsInATextTest < Minitest::Test
     assert_equal ['ab', 'ba'], permutations('ab').sort
     assert_equal ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'], permutations('aabb').sort
 end
+
+
+# In this kata you have to create all permutations of an input
+# string and remove duplicates, if present. This means, you have
+# to shuffle all letters from the input in all possible orders.
+
+# Examples:
+
+# permutations('a'); # ['a']
+# permutations('ab'); # ['ab', 'ba']
+# permutations('aabb'); # ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+# The order of the permutations doesn't matter.
