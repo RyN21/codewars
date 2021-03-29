8e9d0173c40ca664132783401e29ac09819c3b2c
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-28 19:19:52 -0600

    Edit

--- ruby/test/permutations_test.rb
+++ ruby/test/permutations_test.rb
@@ -1,12 +1,13 @@
 gem 'minitest', '~>5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/most_frequently_used_words_in_a_text'
+require_relative '../lib/permutations'
 
 class MostFrequentlyUsedWordsInATextTest < Minitest::Test
   def test_it
     assert_equal ['ab', 'ba'], permutations('ab').sort
     assert_equal ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'], permutations('aabb').sort
+  end
 end
 
 
