1ea5d65bb51e84ac94669359be76e12a43874eb4
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-29 19:00:12 -0600

    Restate the problem

--- ruby/lib/permutations.rb
+++ ruby/lib/permutations.rb
@@ -3,8 +3,8 @@ require 'pry'
 # MY SOLUTION
 #=======================================================================================
 
-def permutations(string)
-
+def permutations(s)
+  binding.pry
 end
 
 # BEST PRACTICE
@@ -21,3 +21,8 @@ end
 
 # PSUEDO CODE
 #=======================================================================================
+
+-The goal here is to take a string and come up with all the different
+possible combinations based on the letters provided in the string
+-once all the possible combinations are found, place them in an array
+-remove duplicates, if present
