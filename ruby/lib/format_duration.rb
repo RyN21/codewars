8aa58264babb4c4741f8dd1100f3a6f5d328824c
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-05-05 21:49:53 -0600

    stopping for the night

--- ruby/lib/format_duration.rb
+++ ruby/lib/format_duration.rb
@@ -4,7 +4,7 @@ require 'pry'
 #=======================================================================================
 
 def format_duration(s)
-
+  binding.pry
 end
 # BEST PRACTICE
 #=======================================================================================
@@ -25,17 +25,21 @@ end
 # PSEUDO CODE
 #=======================================================================================
 
-This kata, I will have to write a functions that takes in an amount of seconds
-and then converts it to a human friendly way to read it
-
-Example (5) will convert to "5 seconds"
-I am taking in an integer and returning a string
-
--the first step will be converting the number of seconds to years:days:hours:minutes:seconds
--then we can take the new conversion and interpolate it into a string
-  -may contain if statements
-
-other things to note
--must be a non-negative integer
--if 0 then return 'now'
-  
+# This kata, I will have to write a functions that takes in an amount of seconds
+# and then converts it to a human friendly way to read it
+#
+# Example (5) will convert to "5 seconds"
+# I am taking in an integer and returning a string
+#
+# -the first step will be converting the number of seconds to years:days:hours:minutes:seconds
+# -then we can take the new conversion and interpolate it into a string
+#   -may contain if statements
+# -consider modulo
+#
+# other things to note
+# -must be a non-negative integer
+# -if 0 then return 'now'
+#
+# def format_duration(s)
+#
+# end
