88852c8a2ac8b25f462f5465f15ab1e3331dcb37
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-05-05 21:44:48 -0600

    Add some notes

--- ruby/lib/format_duration.rb
+++ ruby/lib/format_duration.rb
@@ -24,3 +24,18 @@ end
 
 # PSEUDO CODE
 #=======================================================================================
+
+This kata, I will have to write a functions that takes in an amount of seconds
+and then converts it to a human friendly way to read it
+
+Example (5) will convert to "5 seconds"
+I am taking in an integer and returning a string
+
+-the first step will be converting the number of seconds to years:days:hours:minutes:seconds
+-then we can take the new conversion and interpolate it into a string
+  -may contain if statements
+
+other things to note
+-must be a non-negative integer
+-if 0 then return 'now'
+  
