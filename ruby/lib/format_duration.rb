2fd4c74caec2bcdaeb9bab35141d57c4a7cda48f
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-05-11 20:55:27 -0600

    Add alternatives

--- ruby/lib/format_duration.rb
+++ ruby/lib/format_duration.rb
@@ -39,17 +39,126 @@ end
 # BEST PRACTICE
 #=======================================================================================
 
+def format_duration(total)
+  if total == 0
+    "now"
+  else
+    duration = {
+      year:   total / (60 * 60 * 24 * 365),
+      day:    total / (60 * 60 * 24) % 365,
+      hour:   total / (60 * 60) % 24,
+      minute: total / 60 % 60,
+      second: total % 60
+    }
+
+    @output = []
+
+    duration.each do |key, value|
+      if value > 0
+        @output << "#{value} #{key}"
+        @output.last << "s" if value != 1
+      end
+    end
+
+    @output.join(', ').gsub(/\,\s(?=\d+\s\w+$)/, " and ")
+  end
+end
+
 
 # CLEVER
 #=======================================================================================
 
+def format_duration(seconds)
+  *init, last = [3600 * 24 * 365, 3600 * 24, 3600, 60, 1]
+                .collect {|x| div, seconds = seconds.divmod(x); div}
+                .reverse
+                .zip([" second", " minute", " hour", " day", " year"])
+                .keep_if {|x, y| x != 0}
+                .map{|x, y| x.to_s + y + (x > 1 ? "s" : "")}
+                .reverse
+  last ? "#{init.join(", ")}#{init == [] ? "" : " and "}#{last}" : "now"
+end
+
 
 # ALTERNATIVE
 #=======================================================================================
 
+def format_duration(seconds)
+  return "now" if seconds == 0
+  m,s = seconds.divmod(60)
+  h,m = m.divmod(60)
+  d,h = h.divmod(24)
+  y,d = d.divmod(365)
+
+  *f,l = {'year'=>y,'day'=>d,'hour'=>h,'minute'=>m,'second'=>s}.to_a.select{|i| i.last > 0}.map{|i| (i.last>1)? "#{i.last} #{i.first+'s'}": "#{i.last} #{i.first}"}
+  (f.count > 0)? f.join(', ') + ' and '+l : l
+end
+
 
 # ALTERNATIVE
 #=======================================================================================
+
+def format_duration(seconds)
+  phrase = [['year', 31536000], ['day', 86400], ['hour', 3600], ['minute', 60], ['second', 1]]
+  phrase.map! do |lable, unit_seconds|
+    unit = seconds / unit_seconds
+    seconds -= unit * unit_seconds
+    ["1 #{lable}", "#{unit} #{lable}s", nil][unit <=> 1]
+  end.compact!
+  [phrase.join, phrase[0..-2].join(', ') + " and #{phrase[-1]}", 'now'][phrase.count <=> 1]
+end
+
+# ALTERNATIVE
+#=======================================================================================
+
+def format_duration(t)
+  s = " %i years, %i days, %i hours, %i minutes, %i seconds,"
+  s %= [t/31536000,t/86400%365,t/3600%24,t/60%60,t%60]
+  s.gsub!(/ 0 \w+,/, '') # remove zero components
+  s.gsub!(/ 1 (\w+)s,/, ' 1 \1,') # fix singular
+  s.gsub!(/, (\d+ \w+),$/, ' and \1,') # insert 'and'
+  s=='' ? 'now' : s[1...-1] # remove leading ' ' and trailing ','
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+def format_duration(seconds)
+  return 'now' unless seconds > 0
+  *init, last = [31536000, 86400, 3600, 60, 1]
+                .collect {|x| units, seconds = seconds.divmod(x); units}
+                .zip([" year", " day", " hour", " minute", " second"])
+                .keep_if {|x, y| x != 0}
+                .map{|x, y| x.to_s + y + (x > 1 ? "s" : "")}
+
+  init.size == 0 ? last : "#{init.join(", ")} and #{last}"
+end
+
+
+# ALTERNATIVE
+#=======================================================================================
+
+YEAR = 31536000
+DAY = 86400
+HOUR = 3600
+MINUTE = 60
+
+def format_duration(seconds)
+  return "now" if seconds.zero?
+
+  time = {}
+  time[:year], seconds = seconds.divmod YEAR
+  time[:day], seconds = seconds.divmod DAY
+  time[:hour], seconds = seconds.divmod HOUR
+  time[:minute], seconds = seconds.divmod MINUTE
+  time[:second] = seconds
+  time.reject! { |k,v| v.zero? }
+
+  time.inject([]) do |out, (k, v)|
+    out.push "#{v} #{k}" + ("s" if v > 1).to_s
+  end.join(', ').sub(/,([^,]+)$/, ' and\1')
+end
 
 
 # PSEUDO CODE
