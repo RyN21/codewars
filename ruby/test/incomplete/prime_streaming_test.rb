8105acb987338f48fd0f66ec15ec62a01c36b1c5
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-01 21:18:36 -0600

    move files

--- ruby/test/incomplete/prime_streaming_test.rb
+++ ruby/test/incomplete/prime_streaming_test.rb
@@ -3,6 +3,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/prime_streaming'
 
+class PrimeStreamingTest < Minitest::Test
+  def test_it
+    assert_equal ['ab', 'ba'], permutations('ab').sort
+    assert_equal ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'], permutations('aabb').sort
+  end
+end
+
+
 def verify(from_n, *vals)
   stream = Primes.stream
 
