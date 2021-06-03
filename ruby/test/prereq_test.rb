94fbc6bf39c2bdb369e8ba609ac520e3fea1908b
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-03 17:00:11 -0600

    Edit test

--- ruby/test/prereq_test.rb
+++ ruby/test/prereq_test.rb
@@ -4,9 +4,29 @@ require 'minitest/pride'
 require_relative '../lib/prereq'
 
 class PrereqTest < Minitest::Test
-  prereq_1 = []
+  prereq_1 = [
+    ["class_1", "class_2"],
+    ["class_4", "class_5"],
+    ["class_2", "class_3"],
+    ["class_5", "class_6"],
+    ["class_3", "class_4"]
+  ]
+
+  prereq_2 = [
+    ["class_1", "class_2"],
+    ["class_2", "class_3"],
+    ["class_4", "class_5"],
+    ["class_3", "class_4"]
+  ]
+
+  prereq_3 = [
+    ["class_1", "class_2"],
+  ]
+
 
   def test_it
-    assert_equal "", prereq(prereq_1)
+    assert_equal "class_3", prereq(prereq_1)
+    assert_equal "class_3", prereq(prereq_2)
+    assert_equal "class_1", prereq(prereq_3)
   end
 end
