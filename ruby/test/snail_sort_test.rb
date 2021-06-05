d3b29c7b2f3d7d4d572ce5f701e8ef3094934d7a
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-05 12:12:07 -0600

    Add test

--- ruby/test/snail_sort_test.rb
+++ ruby/test/snail_sort_test.rb
@@ -5,8 +5,12 @@ require_relative '../lib/snail_sort'
 
 class SnailSortTest < Minitest::Test
   def test_it
-    input = [[1,2,3],[4,5,6],[7,8,9]]
-    output = [1, 2, 3, 6, 9, 8, 7, 4, 5]
-    assert_equal output, snail(input)
+    input1 = [[1,2,3],[4,5,6],[7,8,9]]
+    output1 = [1, 2, 3, 6, 9, 8, 7, 4, 5]
+    assert_equal output1, snail(input1)
+
+    input2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+    output3 = [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+    assert_equal output2, snail(input2)
   end
 end
