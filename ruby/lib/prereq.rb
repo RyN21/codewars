ad90652e515c830b2adcf3f2b3e82b1a74543118
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-04 22:32:36 -0600

    Add sorting method and test passes

--- ruby/lib/prereq.rb
+++ ruby/lib/prereq.rb
@@ -8,6 +8,7 @@ def prereq(prereqs)
   prereqs.each do |p|
     prereq_classes[p[0]] = p[1]
   end.to_h
+  binding.pry
   class_count = prereqs.flatten.uniq.map do |c|
     [c, prereqs.flatten.count(c)]
   end
@@ -19,11 +20,14 @@ def prereq(prereqs)
     prereq_classes[c[0]] != nil
   end
 
-  sorted = 
-  binding.pry
-  sorted_classes = []
-  prereqs.each do |p,c|
+  sorted = [starting_class[0]]
+  until prereq_classes[sorted.last] == nil
+    sorted << prereq_classes[sorted.last]
   end
+
+  half = sorted.count / 2 - 1 if sorted.count.even?
+  half = sorted.count / 2 if sorted.count.odd?
+  sorted[half]
 end
 
 
@@ -31,3 +35,11 @@ end
 
 # PSEUDO CODE
 #=======================================================================================
+
+# To sort the classes:
+# we can start by shoveling the starting class name into sorted array
+# then we can use the last element in the sorted array (the last class added / the first class)
+# this would give us it's value which is the next class in the track
+# We can then shovel that next class in the sorted array.
+# we can then do this until the returned value from the last class added is nil
+# we will first call on the starting class by putting it in as a key
