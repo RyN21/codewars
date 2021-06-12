2425ddc05f47a685f06dbe0b50e3224b18ee0bcf
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-11 19:49:20 -0600

    Add some psuedo code

--- ruby/lib/boolean_order.rb
+++ ruby/lib/boolean_order.rb
@@ -34,41 +34,57 @@ end
 # PSUEDO CODE
 #=======================================================================================
 
-So to start with what xor is:
-  - examples
-    - t t  =  f
-    - t f  =  t
-    - f t  =  t
-    - f f  =  f
+# So to start with what xor is:
+#   - examples
+#     - t t  =  f
+#     - t f  =  t
+#     - f t  =  t
+#     - f f  =  f
+#
+# the goal of this kata is to return the number of arrangements
+# that the evaluate to true
+#
+# the arrangement part is go to be tricky.
+# Arrangements will be based on the postions of the parantheses.
+#
+# an example is:
+# "tft" - "^&"
+#
+# ((t ^ f) & t)
+# (t ^ (f & t))
+#
+# these two arrangements both equal 2 so 2 is returned
+#
+# So im going to assume here with this example that:
+# - parantheses will have to be around 2 booleans only.
+# - in this case there is only 2 options.
+#
+# example 2:
+# "ttftff" , "|&^&&"
+#
+# ((t | t) & (f ^ t) & (f & f))
+# ((t | t) & ((f ^ t) & (f & f)))
+# (((t | t) & (f ^ t)) & (f & f))
+# (t | t & f ^ t & f & f)
+# (t | t & f ^ t & f & f)
+# (t | t & f ^ t & f & f)
+#
+# - there can be so many possible Arrangements
+# - the answer for this is 16.
+# - So there are AT LEAST 16 possible arrangements.
 
-the goal of this kata is to return the number of arrangements
-that the evaluate to true
+As for some psuedo code:
 
-the arrangement part is go to be tricky.
-Arrangements will be based on the postions of the parantheses.
+def solve(s, ops)
+  combine s and ops into one string, possible array
+  start by shoveling first s into new string
+  then first ops into the next string.
+  as these are shoveled into the new string / array
+    - we delete them so we can shovel the next one in an easy fashion
 
-an example is:
-"tft" - "^&"
+  next step is going to be creating arrangements
 
-((t ^ f) & t)
-(t ^ (f & t))
+  next would be solving for the end boolean
 
-these two arrangements both equal 2 so 2 is returned
-
-So im going to assume here with this example that:
-- parantheses will have to be around 2 booleans only.
-- in this case there is only 2 options.
-
-example 2:
-"ttftff" , "|&^&&"
-
-((t | t) & (f ^ t) & (f & f))
-((t | t) & ((f ^ t) & (f & f)))
-(((t | t) & (f ^ t)) & (f & f))
-(t | t & f ^ t & f & f)
-(t | t & f ^ t & f & f)
-(t | t & f ^ t & f & f)
-
-- there can be so many possible Arrangements
-- the answer for this is 16.
-- So there are AT LEAST 16 possible arrangements.
+  finally, we will count all the equations that resulted as TRUE
+end
