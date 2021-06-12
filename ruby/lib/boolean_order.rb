949f2037bba0670b11a3cab452c6d6c715dc1503
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-11 19:44:47 -0600

    Add some notes

--- ruby/lib/boolean_order.rb
+++ ruby/lib/boolean_order.rb
@@ -33,3 +33,42 @@ end
 
 # PSUEDO CODE
 #=======================================================================================
+
+So to start with what xor is:
+  - examples
+    - t t  =  f
+    - t f  =  t
+    - f t  =  t
+    - f f  =  f
+
+the goal of this kata is to return the number of arrangements
+that the evaluate to true
+
+the arrangement part is go to be tricky.
+Arrangements will be based on the postions of the parantheses.
+
+an example is:
+"tft" - "^&"
+
+((t ^ f) & t)
+(t ^ (f & t))
+
+these two arrangements both equal 2 so 2 is returned
+
+So im going to assume here with this example that:
+- parantheses will have to be around 2 booleans only.
+- in this case there is only 2 options.
+
+example 2:
+"ttftff" , "|&^&&"
+
+((t | t) & (f ^ t) & (f & f))
+((t | t) & ((f ^ t) & (f & f)))
+(((t | t) & (f ^ t)) & (f & f))
+(t | t & f ^ t & f & f)
+(t | t & f ^ t & f & f)
+(t | t & f ^ t & f & f)
+
+- there can be so many possible Arrangements
+- the answer for this is 16.
+- So there are AT LEAST 16 possible arrangements.
