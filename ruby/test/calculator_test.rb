02588c14b0f827e54cb46d643715aca720673af8
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-19 14:09:10 -0600

    Add instructions

--- ruby/test/calculator_test.rb
+++ ruby/test/calculator_test.rb
@@ -6,7 +6,7 @@ require_relative '../lib/calculator'
 class CalculatorTest < Minitest::Test
   def test_it
     calc = Calculator.new
-    
+
     assert_equal  20, calc.evaluate("4 * 5")
     assert_equal  0.8, calc.evaluate("4 / 5")
     assert_equal  -1, calc.evaluate("4 - 5")
@@ -14,3 +14,16 @@ class CalculatorTest < Minitest::Test
     assert_equal  34, calc.evaluate("4 + 5 * 6")
   end
 end
+
+
+# Create a simple calculator that given a string of
+# operators (), +, -, *, / and numbers separated by spaces
+# returns the value of that expression
+
+# Example:
+
+# Calculator.new.evaluate("2 / 2 + 3 * 4 - 6") # => 7
+# Remember about the order of operations! Multiplications and
+# divisions have a higher priority and should be performed
+# left-to-right. Additions and subtractions have a lower
+# priority and should also be performed left-to-right.
