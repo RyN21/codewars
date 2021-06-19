892feceb87eb7cddad0443b69f25bdbab22cd97c
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-19 14:17:15 -0600

    Add some notes

--- ruby/lib/calculator.rb
+++ ruby/lib/calculator.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 class Calculator
   def evaluate(string)
-    
+
   end
 end
 
@@ -35,3 +35,21 @@ end
 
 # PSUEDO CODE
 #=======================================================================================
+
+To start we are going to create a new calculator class
+we will add a method to evaluate the string
+
+the class it self does not need to have any iniializers... I do believe
+
+this calculator looks like it will only need to perform the basics:
+  - addition
+  - subtraction
+  - multiplications
+  - division
+order of operation is important, so multiplications and division are frist/has the highest priority
+  - from left-to-right
+then addition and subtraction from left-to-right
+
+
+
+since we are taking in a string, we will need to convert the string to integers and operators
