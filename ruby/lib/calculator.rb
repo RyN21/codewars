55835b5c23c4f216ba98cf65041f22be460e8294
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-19 14:21:56 -0600

    Add more notes

--- ruby/lib/calculator.rb
+++ ruby/lib/calculator.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 class Calculator
   def evaluate(string)
-
+    binding.pry
   end
 end
 
@@ -36,20 +36,24 @@ end
 # PSUEDO CODE
 #=======================================================================================
 
-To start we are going to create a new calculator class
-we will add a method to evaluate the string
+# To start we are going to create a new calculator class
+# we will add a method to evaluate the string
+#
+# the class it self does not need to have any iniializers... I do believe
+#
+# this calculator looks like it will only need to perform the basics:
+#   - addition
+#   - subtraction
+#   - multiplications
+#   - division
+# order of operation is important, so multiplications and division are frist/has the highest priority
+#   - from left-to-right
+# then addition and subtraction from left-to-right
+#
+#
+#
+# since we are taking in a string, we will need to convert the string to integers and operators
 
-the class it self does not need to have any iniializers... I do believe
-
-this calculator looks like it will only need to perform the basics:
-  - addition
-  - subtraction
-  - multiplications
-  - division
-order of operation is important, so multiplications and division are frist/has the highest priority
-  - from left-to-right
-then addition and subtraction from left-to-right
-
-
-
-since we are taking in a string, we will need to convert the string to integers and operators
+# so once we have the string converted to what we need,
+# we can look for all the "*" and "/" first from left-to-right and perform the calculation
+# then we can find all the "+" and "-" and then perform those calculations last
